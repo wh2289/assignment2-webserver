82dd560cae78c325fa20dd65d01ea39cfd47a164
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-02-17 17:57:38 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,4 @@
-# import socket module
 from socket import *
-# In order to terminate the program
-import sys
-
 
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -13,7 +9,7 @@ def webServer(port=13331):
 
   while True:
     #Establish the connection
-    #print('Ready to serve...')
+    print('Ready to serve...')
     connectionSocket, addr = #Fill in start      #Fill in end
     try:
 
